6e432a874a81ec9f1fa3c8c60caa3542c78a2257
Crowdbotics <engineering@crowdbotics.com>
2023-07-10 11:22:34 +0000

    Update store.

---
+++ store/summerfielddevAPI/api.js
@@ -0,0 +1,68 @@
+import axios from "axios"
+const summerfielddevAPI = axios.create({
+  baseURL: "https://summer-field-dev-99489.botics.co",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_docs_schema_retrieve(payload) {
+  return summerfielddevAPI.get(`/api-docs/schema/`, {
+    params: { lang: payload.lang }
+  })
+}
+function api_v1_login_create(payload) {
+  return summerfielddevAPI.post(`/api/v1/login/`, payload)
+}
+function api_v1_signup_create(payload) {
+  return summerfielddevAPI.post(`/api/v1/signup/`, payload)
+}
+function rest_auth_login_create(payload) {
+  return summerfielddevAPI.post(`/rest-auth/login/`, payload)
+}
+function rest_auth_logout_retrieve(payload) {
+  return summerfielddevAPI.get(`/rest-auth/logout/`)
+}
+function rest_auth_logout_create(payload) {
+  return summerfielddevAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(payload) {
+  return summerfielddevAPI.post(`/rest-auth/password/change/`, payload)
+}
+function rest_auth_password_reset_create(payload) {
+  return summerfielddevAPI.post(`/rest-auth/password/reset/`, payload)
+}
+function rest_auth_password_reset_confirm_create(payload) {
+  return summerfielddevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+}
+function rest_auth_registration_create(payload) {
+  return summerfielddevAPI.post(`/rest-auth/registration/`, payload)
+}
+function rest_auth_registration_verify_email_create(payload) {
+  return summerfielddevAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload
+  )
+}
+function rest_auth_user_retrieve(payload) {
+  return summerfielddevAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return summerfielddevAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return summerfielddevAPI.patch(`/rest-auth/user/`, payload)
+}
+export const apiService = {
+  api_docs_schema_retrieve,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_retrieve,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
+}
